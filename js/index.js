3ba587dc3f5565e9383e15039fe142ec147007d0
钟铭 <1257877964@qq.com>
2021-05-10 22:01:35 +0800

    仅修改了thandle的变量 图片未改变

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// 浏览器=》服务器=》浏览器
+var app = require('express')();
+var http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use('/',require('express').static('/Users/zm/3.7 已上传至坚果/store的副本'));
+
+app.get('/', (req, res) => {
+    res.sendFile( '/Users/zm/3.7 已上传至坚果/store的副本/mainEngine.html');
+    // res.sendFile(__dirname + '/img/ii.PNG');
+    // console.log("Request for " + req.url + " received.");
+  });
+var i = 0;
+io.on('connection', (socket) => {
+  // i+=1;
+  // console.log('用户'+i+'进入')
+  console.log('a user connected');
+    // socket.on('chat message', (msg) => {
+    //   io.emit('chat message',msg)
+    // });
+
+    socket.on('thandleclick',(tx)=>{
+        
+        
+        io.emit('thandleclick',tx);
+    });
+
+    // socket.on('btn effect',(ef)=>{
+    //     console.log('effect:',ef);
+    //     io.emit('btn effect',ef);
+    // })
+
+  });
+
+
+
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
